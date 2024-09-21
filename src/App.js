093aac96c0c78e440108843ce4a8d3f41a68db92
shira <shira79@users.noreply.github.com>
2024-09-21 14:40:14 +0900

    勝敗決定ロジック

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ export default function Board() {
     function handleClick(i) {
         const nextSquares = squares.slice();
 
+        if (calculateWinner(squares)) {
+            return;
+        }
+
         if (squares[i]) {
             return;
         }
@@ -20,8 +24,19 @@ export default function Board() {
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
     }
+
+    let status;
+
+    const winner = calculateWinner(squares);
+    console.log(winner)
+    if(winner){
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
     
     return <>
+        <div className="status">{status}</div>
         <div className="board-row">
             <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
             <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -44,4 +59,25 @@ function Square({value, onSquareClick}){
     return <button className="square" onClick={onSquareClick} >
         {value}
     </button>
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for(let i=0; i< lines.length; i++ ){
+        const [a,b,c] = lines[i]
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a]
+        }
+    }
+    return null
 }
